5f253fdf1eeb0687302ca3cda2eb3a99cf1626c3
Andrea Cerra <andrea.cerra@me.com>
2020-08-21 11:13:46 +0200

    Added layout for table row and structured the modules folder

---
+++ SurfingSpots/Modules/Cities/CityRow.swift
@@ -0,0 +1,64 @@
+//
+//  CityRow.swift
+//  SurfingSpots
+//
+//  Created by Andrea Cerra on 8/21/20.
+//  Copyright © 2020 Andrea Cerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct CityRow: View {
+    
+    let city: CityWeather
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            Image(uiImage: city.image)
+                .resizable()
+                .cornerRadius(15.0)
+            VStack(alignment: .leading) {
+                Text(city.name)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                HStack {
+                    Text(city.weather.rawValue)
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                    Text("-")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                    Text(city.temperature.description)
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                    Text("degrees")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                }
+            }.padding()
+        }.frame(minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: 200.0)
+        .padding()
+
+    }
+}
+
+struct CityRow_Previews: PreviewProvider {
+    
+    static var previews: some View {
+    
+        let name = "Cuba"
+        let image = #imageLiteral(resourceName: "test-image-city")
+        let temperature: Float = 28.8
+        let weather = WeatherCondition.Cloudy
+        
+        let city = CityWeather(name: name,
+                               image: image,
+                               temperature: temperature,
+                               weather: weather)
+        
+        return CityRow(city: city)
+    }
+}
