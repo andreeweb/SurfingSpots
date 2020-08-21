085d26175e3775efb96f2c637030d3ed19dc0fad
Andrea Cerra <andrea.cerra@me.com>
2020-08-21 11:08:20 +0200

    Added domain entities

---
+++ SurfingSpots/Entities/CityWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CityWeather.swift
+//  SurfingSpots
+//
+//  Created by Andrea Cerra on 8/21/20.
+//  Copyright © 2020 Andrea Cerra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CityWeather {
+    
+    let name: String
+    let image: UIImage
+    let temperature: Float
+    let weather: WeatherCondition
+}
---
+++ SurfingSpots/Entities/WeatherCondition.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherCondition.swift
+//  SurfingSpots
+//
+//  Created by Andrea Cerra on 8/21/20.
+//  Copyright © 2020 Andrea Cerra. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherCondition: String {
+    case Sunny
+    case Cloudy
+}
